0c400a1e220aa2f1c55fa2ee93a243b90368c031
JpArango <98492814+Jpkeys29@users.noreply.github.com>
2023-07-28 05:58:04 -0400

    Secret key

--- stockapi/stockapi/settings.py
+++ stockapi/stockapi/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-70o(!sfy+36)=yklgr-39*sqa(8p_9rx=^=n5k#s&^gj2h15%9'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
